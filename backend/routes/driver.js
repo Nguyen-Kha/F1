58f42382b416c34251b236a487770f4fa41a2bb2
Nguyen-Kha <khnguyen26@gmail.com>
2021-03-19 20:16:07 -0400

    Rewriting API calls

--- backend/routes/driver.js
+++ backend/routes/driver.js
@@ -2,69 +2,57 @@ const router = require('express').Router();
 let Driver = require('../models/driver.model');
 
 // Get all Drivers
-router.get('/', async(request, response) => {
-    const drivers = await Driver.find({});
-    response.json(drivers);
-    // Either one works
-    // const drivers = await Driver.find()
-    //     .then(drivers => res.json(drivers))
-    //     .catch(err => res.status(400).json('Error: ' + err));
+router.route('/').get((request, response) => {
+    Driver.find({})
+        .then(driver => {
+            response.json(driver);
+        })
+        .catch(err => response.status(400).json('Error: ' + err));
 });
 
 // Get performance for specifc year
-router.get('/year/:id', async(request, response) => {
-    let singleYear = request.params.id; // TODO
-    const yearResults = await Driver.find({year: singleYear});
-    response.json(yearResults);
+router.route('/year/:id').get((request, response) => {
+    let singleYear = request.params.id;
+    Driver.find({year: singleYear})
+        .then(results => {
+            response.json(results);
+        })
+        .catch(err => response.status(400).json('Error: ' + err));
 });
 
 // GET NAME AND PLACE - TESTING UNIT
-router.get('/year/:id/tp/:id2', async(request, response) => {
+router.route('/year/:id/tp/:id2').get((request, response) => {
     let singleYear = request.params.id;
     let singleTP = request.params.id2;
-    const results = await Driver.find({year: singleYear, totalPoints: singleTP})
-    response.json(results)
+    Driver.find({year: singleYear, totalPoints: singleTP})
+        .then(results => {
+            response.json(results);
+        })
+        .catch(err => response.status(400).json('Error: ' + err));
 });
 
 // {year: 2000, results:{ $elemMatch: {race: "AUS", fastestLap:true}}}
 // Get Race Results for certain Year
-router.get('/race/:race_id/year/:year_id', async(request, response) => {
-    let raceName = request.params.race_id;
-    let yearNumber = request.params.year_id;
-    const results = await Driver.find({year: yearNumber, results: {$elemMatch: { race: raceName}}})
-    response.json(results)
+router.route('/race/:race_id/year/:year_id').get((req, res) => {
+    let raceName = req.params.race_id;
+    let yearNumber = req.params.year_id;
+    Driver.find({year: yearNumber, results: {$elemMatch: { race: raceName}}})
+        .then(results => {
+            res.json(results);
+        })
+        .catch(err => res.status(400).json('Error: ' + err));
 });
 
 // Get Single Driver for all years
-router.get('/name/:id', async(request, response) => {
-    // TODO: something to parse for the name, or maybe a body param
+router.route('/name/:id').get((request, response) => {
     let driverName = request.params.id;
-    // console.log(driverName); If we send localhost:3000/api/driver/name/Lewis%20Hamilton
-    // it will print driverName as 'Lewis Hamilton'
-    const nameResults = await Driver.find({name: driverName})
-    response.json(nameResults);
+    Driver.find({name: driverName})
+        .then(results => {
+            response.json(results);
+        })
+        .catch(err => response.status(400).json('Error: ' + err));
 });
 
-// router.route('/').get((req, res) => {
-//     Driver.find()
-//         .then(driver => res.json(driver))
-//         .catch(err => res.status(400).json('Error: ' + err));
-// });
 
-/*
-router.route('/add').post((req, res) => {
-    const year = Number(req.body.year);
-    const name = req.body.name;
-    // Some other stuff
-    const newDriverStat = new Driver({
-        year,
-        name,
-    });
-
-    newDriverStat.save()
-        .then( () => res.json('Driver added'))
-        .catch(err => res.status(400).json('Error: ' + err));
-});
-*/
 
 module.exports = router;
---
+++ backend/routes/driver_alternate.js
@@ -0,0 +1,56 @@
+// Below are the alternate ways we can call the APIs
+// I'm leaving this in here so I have notes for myself
+
+router.get('/', async(request, response) => {
+    const drivers = await Driver.find({});
+    response.json(drivers);
+    // Either one works
+    // const drivers = await Driver.find()
+    //     .then(drivers => res.json(drivers))
+    //     .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.get('/race/:race_id/year/:year_id', async(request, response) => {
+    let raceName = request.params.race_id;
+    let yearNumber = request.params.year_id;
+    const results = await Driver.find({year: yearNumber, results: {$elemMatch: { race: raceName}}})
+    response.json(results)
+});
+
+router.get('/year/:id', async(request, response) => {
+    let singleYear = request.params.id; // TODO
+    const yearResults = await Driver.find({year: singleYear});
+    response.json(yearResults);
+});
+
+router.get('/year/:id/tp/:id2', async(request, response) => {
+    let singleYear = request.params.id;
+    let singleTP = request.params.id2;
+    const results = await Driver.find({year: singleYear, totalPoints: singleTP})
+    response.json(results)
+});
+
+router.get('/name/:id', async(request, response) => {
+    // TODO: something to parse for the name, or maybe a body param
+    let driverName = request.params.id;
+    // console.log(driverName); If we send localhost:3000/api/driver/name/Lewis%20Hamilton
+    // it will print driverName as 'Lewis Hamilton'
+    const nameResults = await Driver.find({name: driverName})
+    response.json(nameResults);
+});
+
+/*
+router.route('/add').post((req, res) => {
+    const year = Number(req.body.year);
+    const name = req.body.name;
+    // Some other stuff
+    const newDriverStat = new Driver({
+        year,
+        name,
+    });
+
+    newDriverStat.save()
+        .then( () => res.json('Driver added'))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+*/
