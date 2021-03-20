e05a3037f306845407ac82457e80e329117b6104
Nguyen-Kha <khnguyen26@gmail.com>
2021-03-20 00:02:44 -0400

    added status codes for basic api calls

--- backend/routes/driver.js
+++ backend/routes/driver.js
@@ -5,7 +5,7 @@ let Driver = require('../models/driver.model');
 router.route('/').get((request, response) => {
     Driver.find({})
         .then(driver => {
-            response.json(driver);
+            response.status(200).json(driver);
         })
         .catch(err => response.status(400).json('Error: ' + err));
 });
@@ -15,7 +15,7 @@ router.route('/year/:id').get((request, response) => {
     let singleYear = request.params.id;
     Driver.find({year: singleYear})
         .then(results => {
-            response.json(results);
+            response.status(200).json(results);
         })
         .catch(err => response.status(400).json('Error: ' + err));
 });
@@ -26,21 +26,31 @@ router.route('/year/:id/tp/:id2').get((request, response) => {
     let singleTP = request.params.id2;
     Driver.find({year: singleYear, totalPoints: singleTP})
         .then(results => {
-            response.json(results);
+            response.status(200).json(results);
         })
         .catch(err => response.status(400).json('Error: ' + err));
 });
 
 // {year: 2000, results:{ $elemMatch: {race: "AUS", fastestLap:true}}}
 // Get Race Results for certain Year
-router.route('/race/:race_id/year/:year_id').get((req, res) => {
-    let raceName = req.params.race_id;
-    let yearNumber = req.params.year_id;
+router.route('/race/:race_id/year/:year_id').get((request, response) => {
+    let raceName = request.params.race_id;
+    let yearNumber = request.params.year_id;
     Driver.find({year: yearNumber, results: {$elemMatch: { race: raceName}}})
         .then(results => {
-            res.json(results);
+            response.status(200).json(results);
         })
-        .catch(err => res.status(400).json('Error: ' + err));
+        .catch(err => response.status(400).json('Error: ' + err));
+});
+
+// Get historical results for specific Race
+router.route('/race/:id').get((request, response) => {
+    let raceName = request.params.id;
+    Driver.find({ "results.race": raceName})
+        .then(results => {
+            response.status(200).json(results);
+        })
+        .catch(err => response.status(400).json('Error: ' + err));
 });
 
 // Get Single Driver for all years
@@ -48,11 +58,9 @@ router.route('/name/:id').get((request, response) => {
     let driverName = request.params.id;
     Driver.find({name: driverName})
         .then(results => {
-            response.json(results);
+            response.status(200).json(results);
         })
         .catch(err => response.status(400).json('Error: ' + err));
 });
-
-
 
 module.exports = router;
